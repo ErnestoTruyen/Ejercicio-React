ac3c9eaae555274373aa9a69ab425dbb22fdebd7
Ernesto Truyen <ernestoseisa@outlook.com>
2019-12-14 15:57:30 -0300

    cambio de prueba

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
+const element = <h1>Hello, Platzi Badgees</h1>;
 ReactDOM.render(element, container);
